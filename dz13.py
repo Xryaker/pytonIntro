4eef88e86cbefde9a6defdc72d49dc9ebc267b75
Xryaker <>
2022-01-28 14:20:39 +0200

    -dz13

---
+++ dz13.py
@@ -0,0 +1,4 @@
+from functions.geometcrifunctions import square_func
+
+if __name__ == '__main__':
+    print(square_func(input()))
---
+++ functions/geometcrifunctions.py
@@ -0,0 +1,3 @@
+def square_func(x):
+    res = int(x)
+    return res * 4, (2 * res ** 2) ** .5, res * res
